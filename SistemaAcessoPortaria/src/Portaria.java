4bf274794596b1935b92d1d7258abb25530620c2
SidemarRosa <sidr.com.slr@gmail.com>
2024-09-10 23:56:12 -0300

    conexão com db

---
+++ SistemaAcessoPortaria/src/Portaria.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Portaria {
+    // Conexão com o banco de dados
+    private static Connection connect() {
+        String url = "jdbc:sqlite:portaria.db"; // Nome do arquivo do banco de dados
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    // Metodo main
+    public static void main(String[] args) {
+        
+    }
+}
