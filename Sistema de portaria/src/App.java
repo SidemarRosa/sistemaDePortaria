c38ac17e71bee01a5c44cbfe14b7d46646b03566
SidemarRosa <sidr.com.slr@gmail.com>
2024-09-14 23:56:34 -0300

    Update App.java

--- Sistema de portaria/src/App.java
+++ Sistema de portaria/src/App.java
@@ -5,6 +5,11 @@ public class App {
     public void criarEmpresa(){
         System.out.println("Digite o nome da empresa");
         String nome = scanner.nextLine();
-        System.out.println("Digite o cnpj da empresa")
+        System.out.println("Digite o cnpj da empresa");
+        String cnpj = scanner.nextLine();
+        System.out.println("Digite o ramo da empresa");
+        String ramo = scanner.nextLine();
+        System.out.println("Empresa criada com sucesso!");
+        String empresas[] = nome+cnpj+ramo;
     }
 }
